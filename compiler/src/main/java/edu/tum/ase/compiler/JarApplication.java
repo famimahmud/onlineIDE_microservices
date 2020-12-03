392f41d4de6a2617645446de216db1e990506027
Fami <mahmudfami@gmail.com>
2020-12-03 13:01:22 +0000

    Add project exercise 5: Code Compilation API with Spring Boot

---
+++ compiler/src/main/java/edu/tum/ase/compiler/JarApplication.java
@@ -0,0 +1,13 @@
+package edu.tum.ase.compiler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JarApplication.class, args);
+	}
+
+}
---
+++ compiler/src/main/java/edu/tum/ase/compiler/service/CompilerService.java
@@ -0,0 +1,73 @@
+package edu.tum.ase.compiler.service;
+
+import edu.tum.ase.compiler.model.SourceCode;
+import org.springframework.stereotype.Service;
+
+import java.io.*;
+
+@Service
+public class CompilerService {
+    public SourceCode compile(SourceCode sourceCode) throws IOException {
+        sourceCode.setStdout("");
+        boolean error = false;
+        // Programmiersprache für Compiler anhand Extension ermitteln
+        String fileName = sourceCode.getFileName();
+        String language = "";
+        int index = fileName.lastIndexOf('.');
+        if (index > 0) {
+            language = fileName.substring(index + 1);
+        }
+        String compiler = null;
+
+        if (language.equals("java")) {
+            compiler = "javac";
+        } else if (language.equals("c")) {
+            compiler = "gcc";
+        } else {
+            sourceCode.setStderr("This language is not supported!");
+            return sourceCode;
+        }
+
+        try {
+            //SourceCode in Datei schreiben, die kompiliert werden kann
+            File file = new File(fileName);
+            FileWriter writer = new FileWriter(file);
+            writer.write(sourceCode.getCode());
+            writer.flush();
+            writer.close();
+
+            //Datei kompilieren
+            String command = compiler + " " + file.getAbsolutePath();
+            Process p = Runtime.getRuntime().exec(command);
+
+            //Fehlermeldungen prüfen und ggf. in stderr speichern
+            BufferedReader stdError = new BufferedReader(new InputStreamReader(p.getErrorStream()));
+            String stderr = "\n...\n";
+            String log = null;
+            while ((log = stdError.readLine()) != null) {
+                stderr += log + "\n";
+                error = true;
+            }
+            if (!error) {
+                sourceCode.setCompilable(true);
+                sourceCode.setStdout("Compilation was successful!");
+                sourceCode.setStderr("");
+            } else {
+                sourceCode.setStderr(stderr);
+            }
+
+            //evtl. sollte man hier noch die generierten Dateien löschen
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        if (!error) {
+            assert sourceCode.getStdout() == "Compilation was successful!";
+        } else {
+            assert sourceCode.getStderr() == "";
+        }
+        return sourceCode;
+    }
+
+}
