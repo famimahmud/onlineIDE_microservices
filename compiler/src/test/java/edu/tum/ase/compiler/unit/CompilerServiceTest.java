ee965db1173f66a3f1173f489ce62b273d77662b
Anna <anna.kovaleva@tum.de>
2021-01-12 14:49:17 +0100

    Add basic test to test pipeline. Update gitignore with target & idea

---
+++ compiler/src/test/java/edu/tum/ase/compiler/unit/CompilerServiceTest.java
@@ -0,0 +1,13 @@
+package edu.tum.ase.compiler.unit;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CompilerServiceTest {
+
+    @Test
+    void compile() {
+        assertEquals(true,true);
+    }
+}
